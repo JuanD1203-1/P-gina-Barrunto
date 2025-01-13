7d6e8e8bc3d8e0bbac8dfb9b3759d114c4d26daa
Fundación Artística y Cultural Barrunto <facbarrunto52@gmail.com>
2025-01-12 19:39:08 -0500

    Subiendo proyecto terminado

--- public/js/app.js
+++
@@ -1,9 +0,0 @@
-console.log("%c¡Bienvenido a mi página web!", "color: blue; font-size: 20px; font-weight: bold;");
-console.log("Fecha de acceso:", new Date().toLocaleString());
-console.warn("Recuerda revisar el código fuente para aprender más.");
-console.log(`
-    ╔════════════════════════════╗
-    ║ Diseñado por [Tu Nombre]   ║
-    ║  ¡Gracias por visitar!     ║
-    ╚════════════════════════════╝
-`);
---
+++ public/public/js/app.js
@@ -0,0 +1,9 @@
+console.log("%c¡Bienvenido a mi página web!", "color: blue; font-size: 20px; font-weight: bold;");
+console.log("Fecha de acceso:", new Date().toLocaleString());
+console.warn("Recuerda revisar el código fuente para aprender más.");
+console.log(`
+    ╔════════════════════════════╗
+    ║ Diseñado por [Tu Nombre]   ║
+    ║  ¡Gracias por visitar!     ║
+    ╚════════════════════════════╝
+`);
